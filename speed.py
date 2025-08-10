740c3770205e51018db92cbfd16d032521c415bb
youssefgamer129 <youssefgamer1295@gmail.com>
2025-08-10 09:34:44 +0100

    Moved speed.py to root and removed general folder

--- speed.py
+++ speed.py
@@ -1,14 +1,17 @@
-                    #Calculate car speed
-print("Hello, this program calculates the speed of a car")
+                     #Calculate car speed
+print("Hello, this program calculates car speed")
 #Data entry
 Distance = float(input("Enter distance traveled (km) :"))
-
-Time = float(input("Enter time taken (hrs) : "))
+hours= float(input("Enter hours :") )
+minutes = float(input("Enter minutes :"))
+seconds = float(input("Enter seconds :"))
+#Convert time to hours
+Time = hours + minutes / 60 + seconds / 3600
 #Calculate speed
 Speed = Distance / Time    
 #Show result
 print("Your speed is" , Speed , "km/h")
-#Transform to miles
+#Convert to miles
 Speed = Speed / 3.6
 #Show result
 print("Your speed is" , Speed , "m/s" ) 
